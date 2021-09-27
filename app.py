be9b43c81b91b4f08f631faac134e6dfa0e59097
Carrotsky55 <89003832+Carrotsky55@users.noreply.github.com>
2021-09-27 13:32:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'I7rJsM3TFredEVWKs2+fIA7FySrINJGcgtsLbQJZLybcu0ogSB+veBEXnjXUAvH3d1HPMubhgaaZ4Lhgr46+E1MZj2TQb+NRERvv9NYEaymbB5FP1ymgbNRgtFCX0ZTaZMajG//jbsSaY/a4KT5omAdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
